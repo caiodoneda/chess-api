432cb19911d3feae0c836298e06b109c4003a0a5
Caio Bressan Doneda <caiodoneda@gmail.com>
2020-03-21 21:10:06 +0000

    Base board class

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,1 +1,8 @@
-export default class Board {};
+export default class Board {
+    private positions: Array<Array<number>>;
+
+    constructor(size: number) {
+        const rows = Array(size).fill(0);
+        this.positions = Array(size).fill(rows);
+    }
+};
