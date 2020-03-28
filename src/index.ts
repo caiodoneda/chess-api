8b7d2004ed46c43c58d3aa23cb26bfdf84cc48ef
arctouch-caiodoneda <caio.doneda@quizlet.com>
2020-03-28 15:55:08 -0300

    Getting port from env

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import isAlgebraicNotation from './middleware/isAlgebraicNotation';
 import BoardService from './services/boardService';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
